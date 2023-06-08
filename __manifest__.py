caa484b9ed4cb744add885da1a140a72b20ec7e1
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2023-06-08 14:23:43 -0600

    Up

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Todo App',
+    'version': '1.0',
+    'summary': 'Manage your personal tasks',
+    'description': 'This module helps you to manage your personal tasks.',
+    'category': 'Tools',
+    'author': 'Antonio Queb',
+    'website': 'https://www.queb.online',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+        'views/beneficiarios_views.xml',
+    ],
+    'application': True,
+}
