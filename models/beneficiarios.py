69565e89b4db6c338eec90e26bcb673d24c06e6e
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2023-06-08 14:47:48 -0600

    Update

--- models/beneficiarios.py
+++ models/beneficiarios.py
@@ -4,4 +4,4 @@ class Beneficiario(models.Model):
     _description = 'Beneficiario'
     _inherit = 'res.partner'
 
-    
+    is_beneficiary = fields.Boolean(string='Is Beneficiary', default=False)
